53c94a376ba08e1e96cb7831599c0bb4600f0103
patrykks <pskalsk@student.agh.edu.pl>
2017-10-28 19:39:38 +0200

    Implementation of the basic functionality of the service

--- src/main/java/parking/lot/service/ParkingLotService.java
+++ src/main/java/parking/lot/service/ParkingLotService.java
@@ -1,7 +1,23 @@
 package parking.lot.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import parking.lot.entity.ParkingLot;
+import parking.lot.repository.ParkingLotRepository;
 
 @Service
 public class ParkingLotService {
+
+    @Autowired
+    ParkingLotRepository parkingLotRepository;
+
+    public Iterable<ParkingLot> findAll() {
+        return parkingLotRepository.findAll();
+    }
+
+    public ParkingLot findById(Long id) {
+        return parkingLotRepository.findOne(id);
+    }
+
+
 }
