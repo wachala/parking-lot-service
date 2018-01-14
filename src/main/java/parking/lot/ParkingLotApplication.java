f33dc76b86353e7abf99b0c86fed21f0a14fe1a6
Piotr Wachala <piotr.wachala@outlook.com>
2018-01-14 11:54:05 +0100

    AWS deployment poc

--- src/main/java/parking/lot/ParkingLotApplication.java
+++ src/main/java/parking/lot/ParkingLotApplication.java
@@ -3,12 +3,10 @@ package parking.lot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableHystrix
